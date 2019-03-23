2ea832e7bce0723c0185ec685f78b317d389c6ce
EvonDu <298550274@qq.com>
2019-03-23 16:06:09 +0800

    对WebSocket服务进行封装,提供事件机制和客户端标识

---
+++ js/websocket.js
@@ -0,0 +1,59 @@
+var websocket_io = function(sdn,id){
+    // 定义类
+    var self = this;
+    var ws = new WebSocket(sdn);
+
+    // 定义属性
+    this.id = id || Date.now().toString(36);
+    this.onopen = null;
+    this.onclose = null;
+    this.onmessage = null;
+    this.onevent = {};
+
+    // 发送信息
+    this.send = function(event, data){
+        ws.send(JSON.stringify({"__event":event, "data":data}));
+    };
+
+    // 连接成功
+    ws.onopen = function() {
+        // 发送登录信息
+        ws.send(JSON.stringify({"__event":"login", "data":self.id}));
+        // 触发事件
+        if(typeof self.onopen === 'function')
+            self.onopen();
+        //DEBUG
+        //console.log("[*] WebSocket connected.");
+    };
+    // 关闭连接
+    ws.onclose = function() {
+        // 触发事件
+        if(typeof self.close === 'function')
+            self.close();
+        // DEBUG
+        console.log("[*] WebSocket close.");
+    };
+    // 接收信息
+    ws.onmessage = function(event) {
+        //解析报文
+        var json = null;
+        try{
+            json = JSON.parse(event.data);
+        }catch (e){
+            json = null
+        }
+        //判断报文触发事件
+        if(json && json.__event && json.data){
+            // 触发事件
+            if(typeof self.onevent[json.__event] === 'function')
+                self.onevent[json.__event](json.data);
+        } else {
+            // 触发事件
+            if(typeof self.onmessage === 'function')
+                self.onmessage(event.data);
+        }
+    };
+
+    // 返回
+    return this;
+};
--- service/WebSocketService.go
+++ service/WebSocketService.go
@@ -1,15 +1,23 @@
 package service
 
 import (
+	"encoding/json"
 	"go-websocket/core"
 	"net/http"
 	"golang.org/x/net/websocket"
 	"fmt"
 )
 
+//定义客户端
+type Client struct {
+	Id			string
+	Connect		*websocket.Conn
+}
+
 //定义结构体
 type WebSocketService struct {
 	core.BaseWebSocket
+	Clients				[]*Client
 	Config				*core.Config
 }
 
@@ -17,6 +25,7 @@ type WebSocketService struct {
 func (t *WebSocketService) Listen(){
 	//设置服务
 	http.Handle("/", websocket.Handler(t.Handler))
+	http.Handle("/js/", http.FileServer(http.Dir("./")))
 	if t.Config.Test {
 		http.Handle("/test/", http.FileServer(http.Dir("./")))
 	}
@@ -25,30 +34,51 @@ func (t *WebSocketService) Listen(){
 	t.registerEvents(t)
 }
 
-// 客户端事件
+// 原始事件注册
 func (t *WebSocketService) registerEvents(ws *WebSocketService){
-	ws.AddOnConnect(t.onConnect)
-	ws.AddOnClose(t.onClose)
 	ws.AddOnMessage(t.onMessage)
-	ws.AddOnPublish(t.onPublish)
 }
 
-// 客户端连接
-func (t *WebSocketService) onConnect(ws *websocket.Conn) {
-	fmt.Print("[*] Client connect.\n")
-}
-
-// 客户端离线
-func (t *WebSocketService) onClose(ws *websocket.Conn) {
-	fmt.Print("[*] Client close.\n")
-}
-
-// 接受客户端信息
+// 接受客户端信息（按照报文扩展事件）
 func (t *WebSocketService) onMessage(ws *websocket.Conn, data string) {
+	//DEBUG
 	fmt.Print("[*] Receive: " + data + "\n")
+
+	//定义扩展事件方法
+	events := make(map[string]func(ws *websocket.Conn, data interface{}, to interface{}))
+	events["login"] = t.extendOnLogin
+	events["publish"] = t.extendOnPublish
+
+	//解析JSON请求信息
+	var request map[string]interface{}
+	json.Unmarshal([]byte(data), &request)
+
+	//事件处理
+	if request["__event"] != nil && request["data"] != nil {
+		event := request["__event"].(string)
+		data := request["data"]
+		to := request["to"]
+		if events[event] != nil {
+			events[event](ws, data, to)
+		}
+	}
 }
 
-// 服务端发送信息
-func (t *WebSocketService) onPublish(data string) {
-	fmt.Print("[*] Publish: " + data + "\n")
+// 扩展事件：用户登录
+func (t *WebSocketService) extendOnLogin(ws *websocket.Conn, data interface{}, to interface{}) {
+	// 注册客户端
+	id := data.(string)
+	client := Client{Id:id, Connect:ws}
+	t.Clients = append(t.Clients, &client)
+}
+
+// 扩展事件：发布信息
+func (t *WebSocketService) extendOnPublish(ws *websocket.Conn, data interface{}, to interface{}) {
+	// 整理格式
+	result := make(map[string]string)
+	result["__event"] = "publish"
+	result["data"] = data.(string)
+	response,_ := json.Marshal(result)
+	// 发布信息
+	t.BaseWebSocket.Publish(string(response))
 }
